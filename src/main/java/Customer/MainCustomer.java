cce9891d1aafc6fe495ff4a7987daca86e832113
vuvanlinhk3 <vanlinh30122003@gmail.com>
2023-12-05 18:40:20 +0700

    add function fix set width and set height defauld

--- src/main/java/Customer/MainCustomer.java
+++ src/main/java/Customer/MainCustomer.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -14,8 +15,24 @@ public class MainCustomer extends Application {
     }
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainCustomer.class.getResource("CustomerView/Login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainCustomer.class.getResource("CustomerView/Profiles.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1400, 650);
+        // Set minimum width and height
+        stage.setMinWidth(1400);
+        stage.setMinHeight(650);
+
+        // Intercept resizing events
+        scene.widthProperty().addListener((obs, oldVal, newVal) -> {
+            if (newVal.doubleValue() < 1400) {
+                stage.setWidth(1400);
+            }
+        });
+
+        scene.heightProperty().addListener((obs, oldVal, newVal) -> {
+            if (newVal.doubleValue() < 650) {
+                stage.setHeight(650);
+            }
+        });
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();

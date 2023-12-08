98743fe973bd51d2c5c600df1fdf4192bf9d2113
vuvanlinhk3 <vanlinh30122003@gmail.com>
2023-12-08 22:42:52 +0700

    add

--- src/main/java/Customer/Controller/LoginController.java
+++ src/main/java/Customer/Controller/LoginController.java
@@ -1,9 +1,19 @@
 package Customer.Controller;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class LoginController  {
+    @FXML
+    private TextField emailornumber;
+    @FXML
+    private void dangnhapbtn(){
+        String user = emailornumber.getText().toString();
+        if(user  == ""){
 
+        }
+    }
 
 }

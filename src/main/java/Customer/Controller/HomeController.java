b9f434e9ebc24e33d2f8bd3740b7335c192b152c
vuvanlinhk3 <vanlinh30122003@gmail.com>
2023-12-09 09:05:30 +0700

    ahhaahahaha

--- src/main/java/Customer/Controller/HomeController.java
+++ src/main/java/Customer/Controller/HomeController.java
@@ -1,16 +1,63 @@
 package Customer.Controller;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 
-public class HomeController extends Application {
+public class HomeController  {
+    @FXML
+    private TextField startpoin;
 
-    public static void main(String[] args) {
-        launch(args);
+    @FXML
+    private Button btnpupup;
+
+    public HomeController() {
+
     }
 
-    @Override
-    public void start(Stage primaryStage) {
+    @FXML
+    public void bookingclick(){
 
     }
+    @FXML
+    public void Bookedclick(){
+
+    }
+    @FXML
+    public void assessclick(){
+
+    }
+    @FXML
+    public void flightfindclick(){
+
+    }
+    @FXML
+    public void initialize() {
+        // Thiết lập vai trò là combobox
+        btnpupup.setAccessibleRoleDescription("combobox");
+
+        // Mô phỏng dữ liệu trong combobox
+        String[] options = {"Apple", "Banana", "Cherry", "Grapes", "Orange"};
+
+        // Tạo Popup để hiển thị các mục phù hợp
+        Popup popup = new Popup();
+        for (String option : options) {
+            Button optionButton = new Button(option);
+            optionButton.setOnAction(e -> {
+                startpoin.setText(option);
+                popup.hide();
+            });
+            popup.getContent().add(optionButton);
+        }
+
+        // Thêm sự kiện khi người dùng nhấn nút
+        btnpupup.setOnAction(e -> {
+            popup.show(btnpupup.getScene().getWindow(), btnpupup.getScene().getWindow().getX() + btnpupup.localToScene(10, 10).getX(),
+                    btnpupup.getScene().getWindow().getY() + btnpupup.localToScene(0, 0).getY() + btnpupup.getHeight());
+        });
+    }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,9 @@ module planeapp.paneapp {
     opens Customer to javafx.fxml;
     opens Admin to javafx.fxml;
     opens Database to javafx.fxml;
+    opens Customer.Controller to javafx.fxml;
 
+    exports Customer.Controller;
     exports Database;
     exports Admin;
     exports Customer;

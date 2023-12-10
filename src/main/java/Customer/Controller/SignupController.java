76a576e31230d0393ed85931aa594f490d689043
vuvanlinhk3 <vanlinh30122003@gmail.com>
2023-12-10 12:01:00 +0700

    controller update signup

--- src/main/java/Customer/Controller/SignupController.java
+++ src/main/java/Customer/Controller/SignupController.java
@@ -1,16 +1,98 @@
 package Customer.Controller;
 
-import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class SignupController extends Application {
+public class SignupController {
+    @FXML
+    private TextField nameuser_textfield;
+    @FXML
+    private ComboBox <String> Gender_combobox;
+    @FXML
+    private DatePicker birthday_date;
+    @FXML
+    private ComboBox <String> nationality_combobox;
+    @FXML
+    private ComboBox <String> language_combobox;
+    @FXML
+    private TextField address_textfield;
+    @FXML
+    private ComboBox <String> country_combobox;
+    @FXML
+    private ComboBox <String> provinces_combobox;
+    @FXML
+    private TextField city_textfield;
 
-    public static void main(String[] args) {
-        launch(args);
+    @FXML
+    public void initialize(){
+        ObservableList<String> genderOptions = FXCollections.observableArrayList("Nam", "Nữ", "Khác");
+        Gender_combobox.setItems(genderOptions);
+
+        // tạo chuỗi
+        String [] ArrayCountries = { "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria",
+                "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan",
+                "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia",
+                "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo (Congo-Brazzaville)", "Costa Rica",
+                "Croatia", "Cuba", "Cyprus", "Czechia", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt",
+                "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Fiji", "Finland", "France", "Gabon",
+                "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana",
+                "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel",
+                "Italy", "Ivory Coast", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan",
+                "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar",
+                "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia",
+                "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar (formerly Burma)", "Namibia", "Nauru", "Nepal",
+                "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "North Macedonia (formerly Macedonia)", "Norway", "Oman", "Pakistan",
+                "Palau", "Palestine State", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar",
+                "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe",
+                "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia",
+                "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria",
+                "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan",
+                "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States of America", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City (Holy See)",
+                "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"
+        };
+        ObservableList<String> countries = FXCollections.observableArrayList(ArrayCountries);
+        FXCollections.sort(countries);
+
+        String [] provincesArray = {
+                "An Giang", "Bà Rịa - Vũng Tàu", "Bắc Giang", "Bắc Kạn", "Bạc Liêu", "Bắc Ninh", "Bến Tre", "Bình Định", "Bình Dương", "Bình Phước",
+                "Bình Thuận", "Cà Mau", "Cao Bằng", "Đắk Lắk", "Đắk Nông", "Điện Biên", "Đồng Nai", "Đồng Tháp", "Gia Lai", "Hà Giang",
+                "Hà Nam", "Hà Tĩnh", "Hải Dương", "Hậu Giang", "Hòa Bình", "Hưng Yên", "Khánh Hòa", "Kiên Giang", "Kon Tum", "Lai Châu",
+                "Lâm Đồng", "Lạng Sơn", "Lào Cai", "Long An", "Nam Định", "Nghệ An", "Ninh Bình", "Ninh Thuận", "Phú Thọ", "Quảng Bình",
+                "Quảng Nam", "Quảng Ngãi", "Quảng Ninh", "Quảng Trị", "Sóc Trăng", "Sơn La", "Tây Ninh", "Thái Bình", "Thái Nguyên", "Thanh Hóa",
+                "Thừa Thiên Huế", "Tiền Giang", "Trà Vinh", "Tuyên Quang", "Vĩnh Long", "Vĩnh Phúc", "Yên Bái", "Phú Yên", "Cần Thơ", "Đà Nẵng", "Hải Phòng", "Hà Nội", "Hồ Chí Minh"
+        };
+        ObservableList<String> provinces = FXCollections.observableArrayList(provincesArray);
+        FXCollections.sort(provinces);
+        // lấy sự kiện
+        nationality_combobox.setItems(countries);
+        language_combobox.setItems(countries);
+        country_combobox.setItems(countries);
+        country_combobox.valueProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                // Khi giá trị của countryComboBox thay đổi
+                System.out.println("Selected Country: " + newValue);
+
+                // Kiểm tra giá trị và cập nhật provincesComboBox
+                if ("Vietnam".equals(newValue)) {
+                    provinces_combobox.setItems(provinces);
+                } else {
+                    provinces_combobox.setItems(FXCollections.observableArrayList("Không có thông tin về thành phố của quốc gia này"));
+                }
+            }
+        });
+
+    };
+    @FXML
+    private void next_click(){
+
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
 }

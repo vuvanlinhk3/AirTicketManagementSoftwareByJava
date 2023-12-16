20c94cddf4e5abdaec886d2233c67dd0baca9548
beobr <kimvn123z@gmail.com>
2023-12-16 16:42:18 +0700

    lỗi tùm lum

--- src/main/java/Customer/Controller/AccountFindController.java
+++ src/main/java/Customer/Controller/AccountFindController.java
@@ -1,80 +1,118 @@
 package Customer.Controller;
 
 import Database.DatabaseContection;
-import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-import javax.swing.text.TabableView;
-import java.awt.*;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
-
 public class AccountFindController {
+
     @FXML
     private TextField phone_find_text;
     @FXML
-    private TabableView account_find;
+    private TableView<Account> account_find;
+    @FXML
+    private TableColumn<Account, String> nameclColumn;
 
     private String phone_find;
 
-    // chuyển form
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
+    private ObservableList<Account> accountList = FXCollections.observableArrayList();
 
-//    lấy dữ liệu từ TextField phone_find
-    private boolean isNullText(){
-        phone_find = phone_find_text.getText();
-        if(phone_find.isEmpty()){
-            return true;
-        }
-        return false;
-    }
     @FXML
-    public static boolean getPassengerForPhone(String phone_find, String password) {
-        String sql = "SELECT * FROM Passengers WHERE phone_number = ? AND password = ?";
+    private void click_phone_find(ActionEvent event) {
+        if (isNullText()) {
+            showAlert("Lỗi", "Vui lòng nhập số điện thoại.");
+            return;
+        }
+
+        if (getPassengerForPhone(phone_find)) {
+            // Nếu có khách hàng, hiển thị thông tin trong TableView
+            displayCustomerInfoInTableView();
+        } else {
+            // Nếu không có khách hàng, hiển thị thông báo
+            showAlert("Thông báo", "Không có tài khoản cho số điện thoại này!");
+        }
+    }
+
+    private void displayCustomerInfoInTableView() {
+        // Xóa dữ liệu hiện tại trong TableView
+        accountList.clear();
+
+        // Thêm dữ liệu mới từ truy vấn vào TableView
+        account_find.setItems(accountList);
+        nameclColumn.setCellValueFactory(cellData -> cellData.getValue().nameclProperty());
+    }
+
+    private boolean isNullText() {
+        phone_find = phone_find_text.getText();
+        return phone_find.isEmpty();
+    }
+
+    private boolean getPassengerForPhone(String phone_find) {
+        String sql = "SELECT * FROM Passengers WHERE phone_number = ?";
         try (Connection connection = DatabaseContection.getConnettion();
              PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, phone_find);
-            preparedStatement.setString(2, password);
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                if (resultSet.next()) {
-                    // Retrieve customer information
+                while (resultSet.next()) {
+                    // Lấy thông tin khách hàng và thêm vào danh sách
                     int passengerId = resultSet.getInt("passenger_id");
-                    String name = resultSet.getString("name");
-                    // Add more fields as needed
+                    String namecl = resultSet.getString("namecl");
+                    // Thêm nhiều trường khác nếu cần
 
-                    // Display customer information (you can customize this part)
-                    LoginController.displayCustomerInfo(passengerId, name);
-
-                } else {
-                    // No matching record found
-//                    SignupPassController.showAlert("Lỗi","Invalid email or password.");
-                    return false;
+                    // Thêm thông tin vào danh sách
+                    accountList.add(new Account(namecl));
                 }
+                return !accountList.isEmpty(); // Trả về true nếu có khách hàng
             }
         } catch (SQLException e) {
             e.printStackTrace();
+            return false;
         }
-        return true;
-    }    @FXML
+    }
+
+    private void showAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    public static class Account {
+        private final String namecl;
+
+        public Account(String namecl) {
+            this.namecl = namecl;
+        }
+
+        public String getNamecl() {
+            return namecl;
+        }
+    }
+
+    @FXML
     private void back_click(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/Customer/CustomerView/Home.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-
 }
